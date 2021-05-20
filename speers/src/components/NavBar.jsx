d8155c1e8e04d98d5bccf54bd472f6089863d02b
masinette <masinette.ts@gmail.com>
2021-05-20 18:19:04 -0400

    layout navbar

--- speers/src/components/NavBar.jsx
+++ speers/src/components/NavBar.jsx
@@ -1,28 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown, Button } from 'react-bootstrap';
+import TryItButton from "./buttons/TryItButton";
+
 
 export default function NavBar() {
   return (
     <div>
-      <p>NavBar</p>
 <Navbar bg="light" expand="lg">
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#link">Link</Nav.Link>
-      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+
+
+      <NavDropdown title="&#9776;" id="basic-nav-dropdown">
+        <NavDropdown.Item as={Link} to="/">Home</NavDropdown.Item>
+        <NavDropdown.Item as={Link} to="/">What is it?</NavDropdown.Item>
+        <NavDropdown.Item as={Link} to="/">Perks</NavDropdown.Item>
+        <NavDropdown.Item as={Link} to="/pricing">Pricing</NavDropdown.Item>
       </NavDropdown>
+      <Navbar.Brand as={Link} to="/">EXP|CON</Navbar.Brand>
     </Nav>
+      <Navbar.Brand as={TryItButton}></Navbar.Brand>
+
   </Navbar.Collapse>
-       <Link to="/"><button type="button">Home</button></Link>
+       {/* <Link to="/"><button type="button">Home</button></Link> */}
 </Navbar>
     </div>
   )
