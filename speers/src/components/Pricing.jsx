4eb04d001c59308ae91a7d8d34a88916d90acdcd
masinette <masinette.ts@gmail.com>
2021-05-20 15:31:46 -0400

    install bootstrap

--- speers/src/components/Pricing.jsx
+++ speers/src/components/Pricing.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import TryItButton from "./buttons/TryItButton";
 
 export default function Pricing() {
   return (
     <div>
       <p>Pricing</p>
+      <TryItButton />
     </div>
   )
 }
---
+++ speers/src/components/buttons/TryItButton.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button} from 'react-bootstrap'
+
+export default function TryItButton() {
+  return (
+    <div>
+      <Button variant="outline-success">Success</Button>{' '}
+    </div>
+  )
+};
