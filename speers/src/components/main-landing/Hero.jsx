7e2570910e8968b1f9ebfdbe22de4c6b0ef97b9f
masinette <masinette.ts@gmail.com>
2021-05-21 02:12:36 -0400

    add hero images to bootsrtrap carousel

--- speers/src/components/main-landing/Hero.jsx
+++ speers/src/components/main-landing/Hero.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React from 'react';
 import './main-landing.css';
+import NavBar from "../NavBar";
+import { Carousel } from "react-bootstrap";
+import firstImage from "../../images/image1.png";
+import secondImage from "../../images/image2.png";
+import thirdImage from "../../images/image3.png";
+
 
 export default function Hero() {
   return (
-    <div>Hero</div>
+    <div class="hero-bg-img">
+      <NavBar/>
+      <Carousel fade>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={firstImage}
+            alt="First slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={secondImage}
+            alt="Second slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={thirdImage}
+            alt="Third slide"
+          />
+        </Carousel.Item>
+      </Carousel>
+    </div>
   )
 }
