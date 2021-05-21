a0ed61674fa95c4f1cf7644058376db8d500d37d
masinette <masinette.ts@gmail.com>
2021-05-20 20:41:09 -0400

    add PERKS content

--- speers/src/components/main-landing/Perks.jsx
+++ speers/src/components/main-landing/Perks.jsx
@@ -1,12 +1,37 @@
 import React from 'react'
 import './main-landing.css';
 import NavBar from "../NavBar";
+import { Container, Row, Col } from "react-bootstrap";
 
 export default function Perks() {
   return (
-    <div class="perks-bg-color">
+    <div>
       <NavBar/>
-      Perks
+    <div class="perks-bg-color">
+
+      <h2 class="perks-section-title">Perks</h2>
+
+
+      <Container>
+        <Row>
+          <Col>
+            <p class="perks-col-line-break">___________</p>
+            <h3 class="perks-col-title">Subscription Payment Model</h3>
+            <p class="perks-col-text">No commitment, cancel anytime. Never worry about forgetting a payment again!</p>
+          </Col>
+          <Col>
+            <p class="perks-col-line-break">___________</p>
+            <h3 class="perks-col-title">No Fee Cancelation Policy</h3>            
+            <p class="perks-col-text">No commitment, cancel anytime. Never worry about forgetting a payment agai!</p>
+          </Col>
+          <Col>
+            <p class="perks-col-line-break">___________</p>
+            <h3 class="perks-col-title">Subscription Payment Model</h3>
+            <p class="perks-col-text">No commitment, cancel anytime. Never worry about forgetting a payment again!</p>
+          </Col>
+        </Row>
+      </Container>
+    </div>
     </div>
   )
 }
