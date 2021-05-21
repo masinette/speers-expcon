7152e8961d0c39424e0faa38eb03f8faa04c8ab0
masinette <masinette.ts@gmail.com>
2021-05-21 03:19:36 -0400

    add hero content

--- speers/src/components/main-landing/Hero.jsx
+++ speers/src/components/main-landing/Hero.jsx
@@ -5,33 +5,50 @@ import { Carousel } from "react-bootstrap";
 import firstImage from "../../images/image1.png";
 import secondImage from "../../images/image2.png";
 import thirdImage from "../../images/image3.png";
+import TryItButton from "../buttons/TryItButton";
 
 
 export default function Hero() {
+
   return (
     <div class="hero-bg-img">
-      <NavBar/>
+          <NavBar/>
       <Carousel fade>
-        <Carousel.Item>
+        <Carousel.Item interval={10000}>
           <img
             className="d-block w-100"
             src={firstImage}
             alt="First slide"
-          />
+            />
+            <TryItButton/>
+          <Carousel.Caption >
+            <h3>INTERACTIVE CONCERT EXPERIENCE</h3>
+            <p>Experience your favourite artists like never before and from the comfort of your own home.</p>
+          </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
+
+        <Carousel.Item interval={1000}>
           <img
             className="d-block w-100"
             src={secondImage}
             alt="Second slide"
           />
+          <Carousel.Caption>
+            <h3>INTERACTIVE CONCERT EXPERIENCE</h3>
+            <p>Experience your favourite artists like never before and from the comfort of your own home.</p>
+          </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
+
+        <Carousel.Item interval={1000}>
           <img
             className="d-block w-100"
             src={thirdImage}
             alt="Third slide"
           />
+          <Carousel.Caption>
+            <h3>INTERACTIVE CONCERT EXPERIENCE</h3>
+            <p>Experience your favourite artists like never before and from the comfort of your own home.</p>
+          </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
     </div>
