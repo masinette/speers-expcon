6d22060666124fb688b33f5eebc8ba5bcfa80c29
masinette <masinette.ts@gmail.com>
2021-05-21 22:13:01 -0400

    test redeploy

--- speers-expcon/src/App.js
+++ speers-expcon/src/App.js
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <div className="App">
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         {/* <NavBar/> */}
         <Switch>
           <Route path="/" exact component={() => <Main />} />
